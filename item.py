e9790d21814d81ed6fdc365e0855648e36f35d54
Kevin Stradtman <stradtkt@gmail.com>
2018-11-01 14:48:43 -0400

    added post items with a new class method

--- item.py
+++ item.py
@@ -7,6 +7,13 @@ from sercurity import *
 class Item(Resource):
     @jwt_required
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found.'}
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "SELECT * FROM items WHERE name=?"
@@ -15,14 +22,19 @@ class Item(Resource):
         connection.close()
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found.'}
+
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': "An item with the name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
         return item, 201
 
     def delete(self, name):
