28504ed47c727931bcebb8156571c9252ad5e5ed
Kevin Stradtman <stradtkt@gmail.com>
2018-11-01 13:12:04 -0400

    find user by username

--- user.py
+++ user.py
@@ -1,5 +1,20 @@
+import sqlite3
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = User(row[0], row[1], row[2])
+        else:
+            user = None
+        connection.close()
+        return user
