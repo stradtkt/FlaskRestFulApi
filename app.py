14991358d73d84815e9a6a31ad57961eeb0b7907
Kevin Stradtman <stradtkt@gmail.com>
2018-11-01 14:38:39 -0400

    added a new table and changed the item get method

--- app.py
+++ app.py
@@ -1,48 +1,18 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from sercurity import *
 from user import UserRegister
+from item import *
+import create_tables
 
 app = Flask(__name__)
 app.secret_key = "dflkdhfldkfhdlfkhdfldhfkldhf"
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
-items = []
 
-class Item(Resource):
-    @jwt_required
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
 
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with the name '{}' already exists.".format(name)}, 400
-        data = request.get_json()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        data = request.get_json()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
 
 api.add_resource(Item, '/item<string:name>')
 api.add_resource(ItemList, '/items')
--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -48,6 +50,8 @@ class UserRegister(Resource):
     )
     def post(self):
         data = UserRegister.parser.parse_args()
+        if User.find_by_username(data['username']):
+            return {'message': 'Username already exists, try again.'}
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "INSERT INTO users VALUES (NULL, ?, ?)"
