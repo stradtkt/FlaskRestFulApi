d850ceeaa9bbc5e6715ac0d9d6a23d7030c48326
Kevin Stradtman <stradtkt@gmail.com>
2018-11-01 13:16:29 -0400

    find user by id

--- user.py
+++ user.py
@@ -18,3 +18,17 @@ class User:
             user = None
         connection.close()
         return user
+        
+    @classmethod
+    def find_by_id(cls, _id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE id=?"
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
